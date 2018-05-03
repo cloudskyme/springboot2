e8975c106910428c60b22825bbda56cdaaeaccd8
zhangfeng <zhangfeng@DESKTOP-E3I9LR5>
2018-05-03 09:46:43 +0800

    自定义配置

---
+++ 02configuration/src/main/java/com/cloud/skyme/TestBean.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.cloud.skyme;
+
+/**
+ * @author zhangfeng
+ *
+ */
+public class TestBean {
+	private String username;
+    private String url;
+    private String password;
+
+    public void sayHello() {
+        System.out.println("TestBean sayHello...");
+    }
+
+    public String toString() {
+        return "username:" + this.username + ",url:" + this.url + ",password:" + this.password;
+    }
+
+    public void start() {
+        System.out.println("TestBean 初始化。。。");
+    }
+
+    public void cleanUp() {
+        System.out.println("TestBean 销毁。。。");
+    }
+}
